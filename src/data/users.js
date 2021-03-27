dc8b6ed090dc94856516f4bbb24d6f624e07b426
Philipe <philipe_02@hotmail.com>
2021-03-27 16:12:38 -0300

    Fazendo tela de Lista de Usuários

--- src/data/users.js
+++ src/data/users.js
@@ -1,28 +1,28 @@
 export default [
   {
     id: 1,
-    name: "Mônica Silva",
+    name: "Mônica de Sousa",
     email: "monicadaturma@gmail.com",
     avatarUrl:
       "https://www.imagenspng.com.br/wp-content/uploads/2015/06/monica-02.png",
   },
   {
     id: 2,
-    name: "Cascão Santos",
+    name: "Cascão Araújo",
     email: "cascaodaturma@gmail.com",
     avatarUrl:
       "https://3.bp.blogspot.com/-GUhUYIn2E0o/VHHho4Ui7bI/AAAAAAAAgg4/IviokNWSUgM/s1600/turma%2Bda%2Bmonica%2Bem%2Bpng%2B(10).png",
   },
   {
     id: 3,
-    name: "Magali Alves",
+    name: "Magali Fernandes",
     email: "magalidaturma@gmail.com",
     avatarUrl:
       "https://i.pinimg.com/originals/36/64/3d/36643d26e966f2f8897f0c0b6e43aac5.png",
   },
   {
     id: 4,
-    name: "Cebolinha Carvalho",
+    name: "Cebolácio Júnior",
     email: "cebolinhadaturma@gmail.com",
     avatarUrl:
       "https://i.pinimg.com/originals/2c/b1/4e/2cb14ed932192452a69f63f19fe98ee2.png",
--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,8 +1,64 @@
-import React from 'react'
-import {Text} from 'react-native'
+import React from "react";
+import { View, Text, FlatList, Alert } from "react-native";
+import { ListItem, Avatar, Button, Icon } from "react-native-elements";
 
-export default props => {
-  return(
-    <Text>Abigobaldo</Text>
-  )
-}
+import users from "../data/users";
+
+export default (props) => {
+  const confirmUserDeletion = (user) => {
+    Alert.alert("Excluir usuário", "Deseja excluir o usuário?", [
+      {
+        text: "Sim",
+        onPress() {
+          console.warn("Delete " + user.id);
+        },
+      },
+      { text: "Não" },
+    ]);
+  };
+  const getActions = (user) => {
+    return (
+      <>
+        <Button
+          onPress={() => props.navigation.navigate("UserForm", user)}
+          type="clear"
+          icon={<Icon name="edit" size={25} color="orange" />}
+        />
+        <Button
+          onPress={() => confirmUserDeletion(user)}
+          type="clear"
+          icon={<Icon name="delete" size={25} color="red" />}
+        />
+      </>
+    );
+  };
+  const getUserItem = ({ item: user }) => {
+    return (
+      <ListItem
+        key={user.id}
+        bottomDivider
+        onPress={() => {
+          props.navigation.navigate("UserForm", user);
+        }}
+      >
+        <Avatar rounded size="medium" source={{ uri: user.avatarUrl }} />
+        <ListItem.Content>
+          <ListItem.Title style={{ fontSize: 20 }}>{user.name}</ListItem.Title>
+          <ListItem.Subtitle style={{ fontSize: 15 }}>
+            {user.email}
+          </ListItem.Subtitle>
+        </ListItem.Content>
+        {getActions(user)}
+      </ListItem>
+    );
+  };
+  return (
+    <View>
+      <FlatList
+        keyExtractor={(user) => user.id.toString()}
+        data={users}
+        renderItem={getUserItem}
+      />
+    </View>
+  );
+};
