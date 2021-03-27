2ec218063624dcf0e0b4e6819f27d784396fd55d
Philipe <philipe_02@hotmail.com>
2021-03-27 10:37:01 -0300

    Criando estrutura inicial

--- App.js
+++ App.js
@@ -1,65 +1,65 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { StatusBar} from 'expo-status-bar'
-import {NavigationContainer} from '@react-navigation/native'
-import {createStackNavigator} from '@react-navigation/stack'
-import UserList from './src/views/UserList';
-import UserForm from './src/views/UserForm';
-import { Button, Icon } from 'react-native-elements';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import UserList from "./src/views/UserList";
+import UserForm from "./src/views/UserForm";
+import { Button, Icon } from "react-native-elements";
 
+const Stack = createStackNavigator();
 
-
-const Stack = createStackNavigator()
-
-export default props => {
+export default (props) => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName={UserList} screenOptions={screenOptions}>
+      <Stack.Navigator
+        initialRouteName={UserList}
+        screenOptions={screenOptions}
+      >
         <Stack.Screen
           name="UserList"
           component={UserList}
-          options={({navigation}) => {
+          options={({ navigation }) => {
             return {
               title: "Lista de usuários",
               headerRight: () => (
                 <Button
-                  onPress={() => navigation.navigate('UserForm')}
-                  type='clear'
-                  icon={<Icon name='add' size={25} color='white'/>}
+                  onPress={() => navigation.navigate("UserForm")}
+                  type="clear"
+                  icon={<Icon name="add" size={25} color="white" />}
                 />
-              )
-            }
+              ),
+            };
           }}
         />
         <Stack.Screen
           name="UserForm"
           component={UserForm}
           options={{
-            title: "Formulário de usuários"
+            title: "Formulário de usuários",
           }}
         />
       </Stack.Navigator>
-      <StatusBar style='light'/>
+      <StatusBar style="light" />
     </NavigationContainer>
-    
   );
-}
+};
 
 const screenOptions = {
   headerStyle: {
-    backgroundColor: '#f45',
+    backgroundColor: "#f45",
   },
-  headerTintColor:'#EEE',
-  headerTitleStyle:{
-    fontWeight:'bold'
-  }
-}
+  headerTintColor: "#EEE",
+  headerTitleStyle: {
+    fontWeight: "bold",
+  },
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/data/users.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: "Mônica Silva",
+    email: "monicadaturma@gmail.com",
+    avatarUrl:
+      "https://www.imagenspng.com.br/wp-content/uploads/2015/06/monica-02.png",
+  },
+  {
+    id: 2,
+    name: "Cascão Santos",
+    email: "cascaodaturma@gmail.com",
+    avatarUrl:
+      "https://3.bp.blogspot.com/-GUhUYIn2E0o/VHHho4Ui7bI/AAAAAAAAgg4/IviokNWSUgM/s1600/turma%2Bda%2Bmonica%2Bem%2Bpng%2B(10).png",
+  },
+  {
+    id: 3,
+    name: "Magali Alves",
+    email: "magalidaturma@gmail.com",
+    avatarUrl:
+      "https://i.pinimg.com/originals/36/64/3d/36643d26e966f2f8897f0c0b6e43aac5.png",
+  },
+  {
+    id: 4,
+    name: "Cebolinha Carvalho",
+    email: "cebolinhadaturma@gmail.com",
+    avatarUrl:
+      "https://i.pinimg.com/originals/2c/b1/4e/2cb14ed932192452a69f63f19fe98ee2.png",
+  },
+  {
+    id: 5,
+    name: "Chico Bento",
+    email: "chicodaturma@gmail.com",
+    avatarUrl:
+      "https://static.wikia.nocookie.net/monica/images/6/6f/Chico_bento.png/revision/latest?cb=20140930163812&path-prefix=pt-br",
+  },
+];
